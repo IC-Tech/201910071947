03ca164cb5e17428612ab7cb87af86173a8d8dc5
IC-Tech <ic.imesh.chamara@gmail.com>
2019-11-16 11:32:13 +0530

    update gtag

--- src/about.js
+++ src/about.js
@@ -2,9 +2,10 @@
 import './icApp.js'
 import {Theme, initTheme, setTheme} from './Theme.js'
 import {IAR} from './icApp-render.js'
-import {IC_DEV, XHR, pram} from './common.js'
 import './about.scss'
 
+window.ic = window.ic || []
+window.ic.pageLoad = Date.now()
 document.addEventListener('DOMContentLoaded', () => {
 let icApp = ic.icApp
 var _root_ = new icApp.e('#root')
@@ -25,8 +26,6 @@ class IChat extends IAR {
 		}
 		this.analytics = firebase.analytics()
 		this.performance = firebase.performance()
-		var a = firebase.app().options
-		this.functions = IC_DEV ? `http://192.168.8.20:5001/${a.projectId}/${a.locationId}1/` : `https://us-central1-${a.projectId}.cloudfunctions.net/`
 	}
 	didMount() {
 		firebase.auth().onAuthStateChanged(user => {
@@ -41,7 +40,21 @@ class IChat extends IAR {
 				a.b = (a.a.v.id == 'menu-btn' ? 1 : (a.a = a.a.p) ? 0 : 0)
 			if(a.b == 0) icApp.ds({t: 'mb'}).v.checked = false
 		})
+		icApp.qsa('a[data-t="con"]').forEach(a => new icApp.e(a).ae('click', a=> gtag('event', `${(['Website', 'Email', 'Phone'])[(a = new icApp.e(a.target)).d.i]} Contact Click`, {
+  		'event_category': 'click',
+  		'event_label': 'Contact'
+		})))
+		icApp.qsa('a[data-t="sha"]').forEach(a => new icApp.e(a).ae('click', a=> gtag('event', `${(['WhatsApp', 'Facebook', 'Twitter', 'Pinterest', 'Linkedin', 'Email'])[(a = new icApp.e(a.target)).d.i]} Share Click`, {
+  		'event_category': 'click',
+  		'event_label': 'Share'
+		})))
 		this.update({ready:true})
+		;(['page_mount_end', 'About Page Load']).forEach(a => gtag('event', a, {
+  		'name': 'pageMount',
+  		'value': Date.now() - window.ic.pageLoad,
+  		'event_category': 'timing',
+  		'event_label': 'IC App'
+		}))
 	}
 	didUpdate() {}
 	render() {
