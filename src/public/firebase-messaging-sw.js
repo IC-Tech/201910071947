13284cf6ab721dbbaeb64ab26ef58f7ecac5a981
IC-Tech <ic.imesh.chamara@gmail.com>
2019-11-18 10:50:27 +0530

    Update firebase-messaging-sw.js

--- src/public/firebase-messaging-sw.js
+++ src/public/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('/__/firebase/7.3.0/firebase-app.js');
 importScripts('/__/firebase/7.3.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
+importScripts('/ga.js');
 
 firebase.messaging();
